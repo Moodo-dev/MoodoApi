6ee23fff7f46a33551e78a5db375af91d4ad7ff7
Averyden <igo408gt@gmail.com>
2025-06-23 12:01:03 +0200

    import new dependencies from express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+import {Router, request, Response} from "express";
 import express from "express";
 import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 5001;
 const version = 0.1
+
+
 
 app.use(bodyParser.json());
 
