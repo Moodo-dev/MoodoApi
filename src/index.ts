c9687613491b952b8259461741dd1fa5f7642d15
Averyden <igo408gt@gmail.com>
2025-06-23 11:59:01 +0200

    Just run version number through

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 5001;
+const version = 0.1
 
 app.use(bodyParser.json());
 
 app.get("/", (_reg, res) => {
-  res.send("Moodo API is running...");
+  res.send(`Moodo API is running on version ${version}...`);
 });
 
 app.listen(PORT, () => {
