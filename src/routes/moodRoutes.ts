a656c8f5ed9c1d3c006994433e92b999382d4b08
Averyden <igo408gt@gmail.com>
2025-06-24 02:11:27 +0200

    Create post method

--- src/routes/moodRoutes.ts
+++ src/routes/moodRoutes.ts
@@ -8,3 +8,10 @@ let moods: loggedMood[] = [];
 router.get("/", (_req, res) => {
   res.json(moods);
 });
+
+router.post("/", (req, res) => {
+  const newMood = req.body as loggedMood;
+  newMood.id = Math.random().toString(36).substring(2, 8);
+  moods.push(newMood);
+  res.status(201).json(newMood);
+});
