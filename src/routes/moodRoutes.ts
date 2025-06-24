1be53d5c8cd311edba0afd8cbd672040963669fc
Averyden <igo408gt@gmail.com>
2025-06-24 02:25:54 +0200

    Create endpoint to allow for searching for one logged entry

--- src/routes/moodRoutes.ts
+++ src/routes/moodRoutes.ts
@@ -5,9 +5,20 @@ const router = Router();
 
 let moods: loggedMood[] = [];
 
+//! GET ENDPOINTS
+
 router.get("/", (_req, res) => {
   res.json(moods);
 });
+
+router.get("/:id", (req, res) => {
+  const idToSearch = req.params.id;
+  const foundMood = moods.find((m) => m.id === idToSearch);
+  if (foundMood) res.json(foundMood);
+  else res.status(404).json({ message: "Logged mood not found" });
+});
+
+//! POST ENDPOINTS
 
 router.post("/", (req, res) => {
   const newMood = req.body as loggedMood;
