73130bd5e9971fe2c170429cbadd3750fe0ce77a
Averyden <igo408gt@gmail.com>
2025-06-25 18:35:31 +0200

    Allow cors for the specific port

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { router as master } from "./routes";
+import cors from "cors";
 
 const app = express();
 const PORT = 5001;
 const version = 0.1;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(bodyParser.json());
 
